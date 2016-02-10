e039a532012fab33a28a23191e6c9f812489bc7a
bluelotusmedia <jacob@bluelotusmedia.com>
2016-02-09 22:09:58 -0500

    Added sass and played with some style ideas.

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
   </head>
   <body>
 <nav class="navbar navbar-default navbar-fixed-top">
